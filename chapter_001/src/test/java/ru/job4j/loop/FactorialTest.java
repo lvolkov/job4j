7d10a5969d14acf751293d8ee836b0ab4acd4743
lvolkov <l.volkov@list.ru>
2020-01-20 20:49:15 +0300

    5.2. Создать программу, вычисляющую факториал.[#208336]

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        int result = Factorial.calc(5);
+        assertThat(result, is(120));
+    }
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        //напишите здесь тест, проверяющий, что факториал для числа 0 равен 1.
+        int result = Factorial.calc(0);
+        assertThat(result, is(1));
+    }
+}
