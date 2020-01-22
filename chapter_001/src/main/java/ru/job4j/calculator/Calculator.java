e3a1e3fbc70e16e8dbd8334d3963022d3ca71e22
lvolkov <l.volkov@list.ru>
2020-01-22 22:39:11 +0300

    5.8. JavaDoc. Code convention[#208338]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,36 @@
 package ru.job4j.calculator;
 
+/**
+ * Package for calculate task.
+ *
+ * @author Leonid Volkov (l.volkov@list.ru)
+ * @version 1
+ * @since 21.01.2020
+ */
 public class Calculator {
 
     public static void add (double first, double second) {
-        double result =  first + second;
+        double result=first+second;
         System.out.println(first + " + " + second + " = " + result);
     }
     public static void div (double first, double second) {
-        double result =  first / second;
+        double result=first/second;
         System.out.println(first + " / " + second + " = " + result);
     }
     public static void multiply (double first, double second) {
-        double result =  first * second;
+        double result=first*second;
         System.out.println(first + " * " + second + " = " + result);
     }
     public static void subtrack(double first, double second) {
-        double result =  first - second;
+        double result=first-second;
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Конструктор вывода данных в консоль
+     * @param args- first первый аргумент
+     * @param args- second второй аргумент
+     */
     public static void main(String[] args) {
         add (1, 1);
         div (3, 4);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Leonid Volkov (l.volkov@list.ru)
+ * @version 1
+ * @since 22.01.2020
+ *
+ * класс Calculator создан для вычисления
+ * математических операция
+ * сложения,умножения,деления,вычитания
+ */
