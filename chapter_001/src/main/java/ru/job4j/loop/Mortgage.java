da3d32a0ebc2266958d8c33d06bd1288ffeffd94
lvolkov <l.volkov@list.ru>
2020-01-22 23:31:51 +0300

    5.7. Ипотека.[#208332]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,12 +4,10 @@ public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 1;
         double debt= amount+amount*(percent)/100; //годовая задолженность банку
-
         while (salary<debt){
             year++;
             debt=(debt-salary)+(debt-salary)*(percent)/100; // перерасчет задолженности после годовой выплаты
         }
-
         return year;
     }
 }
--- chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -12,14 +12,12 @@ public class MortgageTest {
         int year = mortgage.year(1000, 1200, 25);
         assertThat(year, is(2));
     }
-
     @Test
     public void when3Year() {
         Mortgage mortgage = new Mortgage();
         int year = mortgage.year(150, 120, 50);
         assertThat(year, is(3));
     }
-
     @Test
     public void when1Year() {
         Mortgage mortgage = new Mortgage();
