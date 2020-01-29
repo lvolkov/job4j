a38c2fe0fb8fb0a5ce4df2cffb65dbe430c13917
lvolk <l.volkov@list.ru>
2020-01-29 21:48:39 +0300

    6.3. Массив заполнен true или false[#208277]-ver2

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,17 +3,15 @@ package ru.job4j.array;
  *  проверка, что все элементы в массиве являются или true или false.
  *
  * @author Leonid Volkov (l.volkov@list.ru)
- * @version 1
+ * @version 2
  * @since 27.01.2020
  */
 
 public class Check {
     public static boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
            for (int i=0;i<data.length;i++) {
-               if (data[0] == data[i]) {
-                   result = true;
-               } else {
+               if (data[0] != data[i]) {
                    result = false;
                    break;
                }
