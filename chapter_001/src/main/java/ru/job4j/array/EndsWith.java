18eb630bd114cb8ebb922afc671693c539b2b8ff
lvolk <l.volkov@list.ru>
2020-01-31 21:01:15 +0300

    6.4.1. Слово заканчивается с ...[#208280]-2 версия

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -3,16 +3,22 @@ package ru.job4j.array;
  * метод проверяет, что слово заканчивается определенной последовательностью
  *
  * @author Leonid Volkov (l.volkov@list.ru)
- * @version 1
+ * @version 2
  * @since 30.01.2020
  */
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        String Word = new String (word);
-        String Post = new String (post);
-        if (Word.endsWith(Post)!=true) {
+        // проверить. что массив word имеет последние элементы одинаковые с post
+        for (int i= post.length-1; i>=post.length-1;i--) {
+        if ( word[i+(word.length-post.length)] != post[i]) {
             result = false;
+            break;
+        }
+        //String Word = new String (word);
+        //String Post = new String (post);
+        //if (Word.endsWith(Post)!=true) {
+        //   result = false;
         }
         return result;
     }
