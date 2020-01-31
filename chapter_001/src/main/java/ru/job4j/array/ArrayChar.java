92ed2ee59100ebb986f98c025910e627aa782073
lvolk <l.volkov@list.ru>
2020-01-31 21:11:10 +0300

    6.4. Слово начинается с ...[#208276]-2 версия

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,17 +3,18 @@ package ru.job4j.array;
  * метод проверяет, что слово начинается c определенной последовательности
  *
  * @author Leonid Volkov (l.volkov@list.ru)
- * @version 1
+ * @version 2
  * @since 30.01.2020
  */
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        String Word = new String (word); // переводим текстовый массив word в строку
-        String Pref = new String (pref); // переводим текстовый массив pref в строку
-        if (!Word.startsWith(Pref)) {  // проверяем условие не совпадения начала строки
-            result = false;
+        for (int i=0; i<pref.length; i++) {
+            if (pref[i]!= word [i]) {
+                result=false;
+                break;
+            }
         }
         return result;
     }
