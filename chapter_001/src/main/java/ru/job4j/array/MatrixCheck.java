f55b44b46bae8e2096ded4f54ebb147d66245f4a
lvolk <l.volkov@list.ru>
2020-02-01 13:14:27 +0300

    6.7. Выигрышные комбинации в сокобан[#208278]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,54 @@
+package ru.job4j.array;
+/**
+ * Класс проверяет, существует ли в двухмерной квадратной матрице комбинация символов X
+ * полностью заполняющих вертикаль или горизонталь
+ * @author Leonid Volkov (l.volkov@list.ru)
+ * @version 1
+ * @since 01.02.2020
+ */
+public class MatrixCheck {
+    //создаем метод проверки заполненной горизонтали
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+    //создаем метод проверки заполненной вертикали
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+   // метод создания массива из диагонали,в поиске выигрышной позиции не применяется.
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
+    // создаем метод поиска выигрышной позиции
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                if (monoHorizontal(board, i) == true || monoVertical(board, i) == true) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+
