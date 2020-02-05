b924f9ad6a26b983163cd06fbab2b52f4e15273e
lvolk <l.volkov@list.ru>
2020-02-05 22:30:01 +0300

    6.8. Дефрагментация массива.[#208285] поменял код с циклом while

--- chapter_001/src/main/java/ru/job4j/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -2,23 +2,23 @@ package ru.job4j.array;
 /**
  * метод перемещающий пустые ячейки массива в конец
  * @author Leonid Volkov (l.volkov@list.ru)
- * @version 1
+ * @version 3
  * @since 07.02.2020
  */
 public class Defragment {
     public static String[] compress(String[] array) {
-        int cell = 0; // определяем первую ячейку массива в которую будет произведена запись не пустого значения
+        //int cell = 0; // определяем первую ячейку массива в которую будет произведена запись не пустого значения
         for (int index = 0; index < array.length; index++) {
-            if (array[index] != null) { // определяем номер первой не пустой ячейки
-                String temp = array[cell]; // записываем "в корзину" данные первойй ячейки массива
-                array[cell] = array[index]; // копируем в первую ячейку массива данные из первой найденной заполненной ячейки
-                array[index] = temp; // копируем в освободившуюся ячеку данные из "корзины"
-                cell++; // выбираем для копирования не пустого значения следующую ячейку массива
-                    //int point = index; // указатель, на не null ячейку.*/
-                    // переместить первую не null ячейку
-                    // Здесь нужен цикл while
+                if (array[index] == null) {
+                int point = index; // создаем переменную и присваиваем ей индекс первой пустой ячейки
+                while (array[point] == null && point < array.length-1) {
+                    point++; //определяем первый индекс не нулевой ячейки
+                }
+                String temp = array[index]; // записываем "в корзину" данные первой пустой ячейки массива
+                array[index] = array[point]; // копируем в первую ячейку массива данные из первой найденной заполненной ячейки
+                array[point] = temp; // перемещаем первую не null ячейку
             }
-                // System.out.print(array[index] + " "); // не понятно для чего это команда?
+                 System.out.print(array[index] + " "); // не понятно для чего это команда?
             }
             return array;
         }
