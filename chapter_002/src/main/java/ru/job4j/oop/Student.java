5c50bc5e1974b3d546743e5e55fc077124770a8c
lvolk <l.volkov@list.ru>
2020-02-17 23:21:54 +0300

    1.3. Вызов метода с аргументами.[#208420]_

--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 
 public class Student {
     public void music() {
-        System.out.println("Tra tra tra");
+        System.out.println();
     }
     public void song() {
         System.out.println("I believe I can fly");
