9d77948d22d1d01a9a0cceb7b0e4be912a532abe
lvolk <l.volkov@list.ru>
2020-02-20 20:55:56 +0300

    1.7. Состояние объекта.[#208423]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+/**
+ * создаем конструктор Battery
+ * cоздаем методбкоторый обнуляет значение переменной объекта и увеличивает на
+ * это же значение переменную второго аналогичного объекта
+ * @author Leonid Volkov (l.volkov@list.ru)
+ * @version 1
+ * @since 20.02.2020
+ */
+public class Battery {
+    private int load;
+    public Battery(int charge) {
+        this.load = charge;
+    }
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+    public static void main(String[] args) {
+        Battery first = new Battery (10);
+        Battery second = new Battery (15);
+        System.out.println(first.load + " " + second.load);
+        first.exchange(second);
+        System.out.println(first.load + " " + second.load);
+    }
+}
