d89d8930324e305257a6249bc62f8f3ffeee9d7d
lvolk <l.volkov@list.ru>
2020-02-22 12:39:52 +0300

    2.2. Рефакторинг - Площадь треугольника.[#208414]

--- chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
@@ -1,13 +1,30 @@
 package ru.job4j.condition;
 
 public class TrgArea {
-    public static double area(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        double sqrt = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return sqrt;
+    private Point third;
+    private Point first;
+    private Point second;
+    public TrgArea(Point ap, Point bp, Point cp) {
+            this.first = ap;
+            this.second = bp;
+            this.third = cp;
     }
-    public static void main(String[] args) {
-        double rsl = TrgArea.area(2, 2, 2);
-        System.out.println("area (2, 2, 2) = " + rsl);
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
     }
+    public static boolean exist(double a, double c, double b) {
+        return a + c > b && c + b > a && b + a > c;
+    }
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+             rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
 }
---
+++ chapter_001/src/test/java/ru/job4j/condition/TrgAreaTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TrgAreaTest {
+    @Test
+    public void whenExist() {
+        boolean result = TrgArea.exist(2.0, 2.0, 2.0);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenNoExist() {
+        boolean result = TrgArea.exist(2.0, 2.0, 4.0);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenAreaTrue() {
+        Point first = new Point(0,0);
+        Point second = new Point (0,3);
+        Point third = new Point (2,0);
+        TrgArea trg = new TrgArea(first,second,third);
+        double result = trg.area();
+        int result2 =(int)result;
+        assertThat(result2, is(3));
+    }
+}
