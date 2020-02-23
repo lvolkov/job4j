ca4345115fdcf51a8c7af814e9b58adae26265fe
lvolk <l.volkov@list.ru>
2020-02-23 12:32:38 +0300

    2. Перегрузка конструктора.[#208429] _2 version

--- chapter_002/src/main/java/tracker/Predator.java
+++ chapter_002/src/main/java/tracker/Predator.java
@@ -1,6 +1,9 @@
 package tracker;
 
 public class Predator extends Animal {
+    public Predator(){
+        super();
+    }
     public Predator(String animal) {
         super(animal);
     }
--- chapter_002/src/main/java/tracker/Tiger.java
+++ chapter_002/src/main/java/tracker/Tiger.java
@@ -1,10 +1,12 @@
 package tracker;
 
 public class Tiger extends Predator {
+    public Tiger() {
+        super();
+    }
     public Tiger(String animal) {
         super(animal);
     }
-
     public static void main(String[] args) {
     Tiger simba = new Tiger("tiger");
     }
