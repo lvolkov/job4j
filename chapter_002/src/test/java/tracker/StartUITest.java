a2b3e869424e40703005d31438acbd20c8a735a2
lvolk <l.volkov@list.ru>
2020-03-29 15:07:52 +0300

    7. Шаблон проектирования - Стратегия.[#208401]_2 вер

--- chapter_002/src/test/java/tracker/StartUITest.java
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -40,5 +40,13 @@ public class StartUITest {
         assertThat(deleted, is(IsNull.nullValue()));
     }
 */
-
+   @Test
+   public void whenExit() {
+       StubInput input = new StubInput(
+               new String[] {"0"}
+       );
+       StubAction action = new StubAction();
+       new StartUI().init(input, new Tracker(), new UserAction[] { action });
+       assertThat(action.isCall(), is(true));
+   }
 }
