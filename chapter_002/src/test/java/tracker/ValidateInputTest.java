2719879745da577c82adadbb42e9f8820abaddf5
lvolk <l.volkov@list.ru>
2020-05-04 15:57:39 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#208404]_StubInput2

--- chapter_002/src/test/java/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/tracker/ValidateInputTest.java
@@ -30,9 +30,9 @@ public class ValidateInputTest {
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
         ValidateInput input = new ValidateInput(
-                new StubInput(new String[]{"10","1"})
+                new StubInput(new String[]{"8","1"})
         );
-        input.askInt("Enter");
+        input.askInt("Enter",6);
         assertThat(
                 mem.toString(),
                 is(String.format("Please select key from menu.%n"))
